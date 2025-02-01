084af7344f793e522dd00501ed5ff7f6312ae23b
desecnd <desecnd@protonmail.com>
2025-02-01 21:22:26 +0100

    Add CLI for both sage and python implementations

---
+++ cli-poly.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python3
+from ntru_py.ntc.ntc import NTRU_PARAM_TYPES, NTRU_PARAMS, unpack_ntru_tuple
+from ntru_py.poly.core import ntru_keygen, ntru_random_message, ntru_encrypt, ntru_decrypt
+from ntru_py.ntc.ntc_json import *
+import sys
+
+VALID_CMDS = ['keygen', 'message', 'encrypt', 'decrypt'] 
+# Commands list:
+# encrypt m pk -> c.json
+# decrypt c sk -> c_dec.json 
+# message -> m.json
+# keygen -> pk.json sk.json
+
+def cmd_keygen(ntru_tuple: NtruTuple) -> tuple[PolyCoeffs, PolyCoeffs]:
+    N, p, q, d = ntru_tuple
+    h, f = ntru_keygen(N, p, q, d)
+    return h, f
+
+def cmd_encrypt(ntru_tuple: NtruTuple, m: PolyCoeffs, h: PolyCoeffs) -> PolyCoeffs:
+    N, p, q, d = ntru_tuple
+    c = ntru_encrypt(N, q, d, m, h)
+    return c
+
+def cmd_decrypt(ntru_tuple: NtruTuple, c: PolyCoeffs, f: PolyCoeffs) -> PolyCoeffs:
+    N, p, q, d = ntru_tuple
+    m = ntru_decrypt(N, p, q, c, f)
+    return m
+
+def cmd_message(ntru_tuple: NtruTuple) -> PolyCoeffs:
+    """Generate random polynomial suitable for encryption with coefffs [-p//2: p//2]"""
+    N, p, q, d = ntru_tuple
+    m = ntru_random_message(N, p)
+    return m
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("usage: ./cli-ntru.py ntru-type cmd [params]")
+        print("> types:", NTRU_PARAM_TYPES)
+        print("> cmds:", VALID_CMDS)
+        exit(1)
+
+    ntru_type = sys.argv[1]
+    cmd = sys.argv[2]
+
+    if ntru_type not in NTRU_PARAM_TYPES:
+        print(f"Incorrect ntru-type value '{ntru_type}'. Possible values are: {NTRU_PARAM_TYPES}")
+        exit(1)
+
+    if cmd not in VALID_CMDS:
+        print(f"Incorrect cmd: {cmd}. Possible values are: {VALID_CMDS}")
+
+
+    ntru_dict = NTRU_PARAMS[ntru_type]
+    ntru_tuple = unpack_ntru_tuple(ntru_dict)
+
+
+    if cmd == 'keygen':
+        if len(sys.argv) < 3:
+            print("usage: ./prog.py <ntru-type> keygen [pk.json] [sk.json]")
+            exit(1)
+        
+        h, f = cmd_keygen(ntru_tuple)
+
+        pk_fname = sys.argv[3] if len(sys.argv) >= 4 else f"pk_{ntru_type}.json"
+        sk_fname = sys.argv[4] if len(sys.argv) >= 5 else f"sk_{ntru_type}.json"
+
+        store_pk(h, ntru_tuple, pk_fname)
+        store_sk(f, ntru_tuple, sk_fname)
+
+    if cmd == 'message':
+        if len(sys.argv) != 3:
+            print("usage: ./prog.py <ntru-type> message [message]")
+            exit(1)
+
+        m = cmd_message(ntru_tuple)
+
+        store_message(m, ntru_tuple)
+        
+    if cmd == 'encrypt':
+        if len(sys.argv) != 5:
+            print("usage: ./prog.py <ntru-type> encrypt <m.json> <pk.json>")
+            exit(1)
+
+        m = load_message(ntru_tuple, sys.argv[3])
+        h = load_pk(ntru_tuple, sys.argv[4])
+
+        c = cmd_encrypt(ntru_tuple, m, h)
+
+        store_ciphertext(c, ntru_tuple)
+
+    if cmd == 'decrypt':
+        if len(sys.argv) != 5:
+            print("usage: ./prog.py <ntru-type> decrypt <c.json> <sk.json>")
+            exit(1)
+
+        c: PolyCoeffs = load_ciphertext(ntru_tuple, sys.argv[3])
+        f: PolyCoeffs = load_sk(ntru_tuple, sys.argv[4])
+
+        # ... 
+        m = cmd_decrypt(ntru_tuple, c, f)
+
+        store_message(m, ntru_tuple, "c_dec.json")
+
